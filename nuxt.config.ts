fffe61c2c61088df98894227285692031ecfdd64
Hayato AKAGIRI <87093hayato@gmail.com>
2026-05-03 20:45:54 +0900

    feat: add GTM tags and generate favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,20 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'DUB STATION',
+      script: [
+        {
+          innerHTML:
+            "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K7D89HPW');",
+          tagPosition: 'head',
+        },
+      ],
+      noscript: [
+        {
+          innerHTML:
+            '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7D89HPW" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          tagPosition: 'bodyOpen',
+        },
+      ],
       meta: [
         {
           name: 'theme-color',
@@ -67,6 +81,11 @@ export default defineNuxtConfig({
         {
           rel: 'manifest',
           href: '/manifest.webmanifest',
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/assets/image/favicon.ico',
         },
         {
           rel: 'apple-touch-icon',
